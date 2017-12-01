2272720e77908dfdcf113c29079171fcc56b8395
216k155 <216k155@luxcore.io>
2017-12-02 01:10:28 +1100

    Change base58.cpp/h

--- src/base58.cpp
+++ src/base58.cpp
@@ -1,4 +1,4 @@
-ss
+// Copyright (c) 2017 216k155
 // Copyright (c) 2014-2016 The Bitcoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
